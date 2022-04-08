88831acd979224d4baff2481cca0eaa9736c88d5
Lyubov Bormotova <l.bormotova@leadgid.ru>
2022-04-08 13:56:47 +0300

    init typeorm

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { Connection } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
